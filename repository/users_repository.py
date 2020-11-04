ed9f1986f4aeb01472743a8754186ae0a8c8f224
Reid <rrrelatores@gmail.com>
2020-11-04 12:38:12 -0800

    'Logs'

--- repository/users_repository.py
+++ repository/users_repository.py
@@ -1,5 +1,7 @@
 import pymongo
 import os
+import logging
+from fastapi import HTTPException, status
 
 connection_string = os.environ.get("MONGO_DB_CONN_STR")
 client = pymongo.MongoClient(connection_string)
@@ -12,8 +14,14 @@ def get_users():
     return [{"Name": user['Name'],"Age": user['Age']} for user in db.users.find()]
 
 def post_user(user):
-    db.users.insert(user)
-    return ['Successfully added {} to the database'.format(user["Name"]) for user in db.users.find({"Name": user["Name"]})]
+    try:
+        db.users.insert(user)
+        result = [{user["Name"]} for user in db.users.find({"Name": user["Name"]})]
+        logging.info( f"Successfully created : {result}")
+        return result
+    except Exception as e:
+        logging.warning(f"Unsucessful save: {e}")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to save user.")
 
 def delete_users(user):
     return {
